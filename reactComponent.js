44b5e0eb5fadc030b47a6a7c548c31ca74012554
Stella-42 <66003842+Stella-42@users.noreply.github.com>
2025-04-10 12:17:26 +0300

    Create reactComponent.js

---
+++ reactComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+//we can use JavaScript functions to define a new React component.(function component)
+// Declare a new function component
+function myComponent() {
+   return <h1>Hello world</h1>;
+}
