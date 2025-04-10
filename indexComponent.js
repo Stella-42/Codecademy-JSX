35830b8ab6831ba570a3dcec14c0fc91814ff873
Stella-42 <66003842+Stella-42@users.noreply.github.com>
2025-04-10 12:35:18 +0300

    Update indexComponent.js

--- indexComponent.js
+++ indexComponent.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MyComponent from './App';// alows me to use the function in app.js here
+
+/*<MyComponent>
+  <OtherComponent />
+</MyComponent>
+How to call and nest the component function
+
+*/
+
